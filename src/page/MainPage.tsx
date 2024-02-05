c18834ce364cbfcd5e0252fa4412f8a3a51a2394
park <rmstn133@gmail.com>
2024-02-05 23:42:18 +0900

    swiper test

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
 const MainPage = () => {
+    const handleSlideChange = () => {
+        console.log('slide change');
+    };
+
+    const handleSwiper = (swiper: typeof Swiper) => {
+        console.log(swiper);
+    };
+    
     return (
-        <>Test Code</>
+        <>
+            <div className='main-container'>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    onSlideChange={handleSlideChange}
+                    onSwiper={handleSwiper}
+                    >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                </Swiper>
+            </div>
+        </>
     );
 };
 
