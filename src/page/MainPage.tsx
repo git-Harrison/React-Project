93781c5a98682dceedfcdba5785751f7d13e6f0e
park <rmstn133@gmail.com>
2024-02-15 00:26:55 +0900

    swiper 버전 수정

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import "swiper/components/navigation/navigation.min.css";
-import SwiperCore, { Navigation } from "swiper";
-import 'swiper/swiper-bundle.css';
+import 'swiper/css';
 import '../Main.css';
 
 
@@ -10,25 +8,35 @@ const MainPage = () => {
     const handleSlideChange = () => {
         console.log('slide change');
     };
-    SwiperCore.use([Navigation]);
     const handleSwiper = (swiper: typeof Swiper) => {
         console.log(swiper);
     };
+    const params = {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        spaceBetween: 30
+      }
     return (
         <>
             <div className='main-container'>
                 <Swiper
-                   
-                    >
-                    <div className="swiper-button-prev"></div>
-                    <div className="swiper-button-next"></div>
-                    <SwiperSlide>
-                        <img src="thumnail-01.png" alt="" />
-                    </SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                ...
                 </Swiper>
                 <div>
                     <img src="logo.svg" alt="" />
