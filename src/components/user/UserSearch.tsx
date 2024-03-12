729bdc0d264625c62c9e88780ad305db8ab8d8ab
park <rmstn133@gmail.com>
2024-03-12 18:52:28 +0900

    회원 ouid 조회 성공

--- src/components/user/UserSearch.tsx
+++ src/components/user/UserSearch.tsx
@@ -9,25 +9,55 @@ const UserSearch: React.FC = () => {
     const [nickName, setNickName] = useState("");
     const [searchResult, setSearchResult] = useState<User | null>(null);
 
+    const [info, setInfo] = useState("");
+
     async function searchNickName(){
         try {
-            const response = await axios.get(`https://open.api.nexon.com/fconline/v1/id?nickname=${nickName}`);
+            const apiKey = 'test_6f0c197e84ae4404190434ef3d22e04a0139b68bdef6442e8a85239fe60add9b5adf99ffd0f50f3265899f1955b5c56e';
+            const encodedNickName = encodeURIComponent(nickName);
+            const response = await axios.get(`https://open.api.nexon.com/fconline/v1/id?nickname=${encodedNickName}`,{
+                headers: {
+                    'x-nxopen-api-key': `${apiKey}`,
+                }
+            });
             if (response.data) {
                 const user: User = response.data;
                 setSearchResult(user);
                 console.log("성공함", response);
+                console.log(response.data.ouid);
+                
             } else {
                 console.error("응답 데이터에 사용자 정보가 포함되어 있지 않습니다.");
             }
         } catch (error) {
             console.error("실패함", error);
-            console.log(nickName);
         }
     }
     function handleInput(e: React.ChangeEvent<HTMLInputElement>){
         setNickName(e.target.value);
-        console.log(e.target.value)
-    }
+    } 
+    
+    // async function searchNickName(){
+    //     try {
+    //         const apiKey = 'test_6f0c197e84ae4404190434ef3d22e04a0139b68bdef6442e8a85239fe60add9b5adf99ffd0f50f3265899f1955b5c56e';
+    //         const encodedNickName = encodeURIComponent(nickName);
+    //         const response = await axios.get(`https://open.api.nexon.com/fconline/v1/user/basic?ouid=${encodedNickName}`,{
+    //             headers: {
+    //                 'x-nxopen-api-key': `${apiKey}`,
+    //             }
+    //         });
+    //         if (response.data) {
+    //             const user: User = response.data;
+    //             setSearchResult(user);
+    //             console.log("성공함", response);
+    //         } else {
+    //             console.error("응답 데이터에 사용자 정보가 포함되어 있지 않습니다.");
+    //         }
+    //     } catch (error) {
+    //         console.error("실패함", error);
+    //     }
+    // }
+
     return (
         <div>
             <form action="">
